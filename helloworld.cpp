c03421f365fe751378514ec7f2dc7a6172844a88
huangsitao <980086759@qq.com>
2019-07-27 06:52:48 +0800

    test

---
+++ helloworld.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+int main()
+{
+	CTest test;
+	test.print();
+}
---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include "test.h"
+#include <iostream>
+
+CTest::CTest()
+{
+}
+
+CTest::~CTest()
+{
+}
+
+void CTest::print()
+{
+	std::cout << "hello world!" << std::endl;
+}
