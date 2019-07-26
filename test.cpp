b87dd8a0542bf3becfd8de2f5bcc147021adcb70
huangsitao <980086759@qq.com>
2019-07-27 06:58:31 +0800

    test2

--- test.cpp
+++ test.cpp
@@ -13,3 +13,8 @@ void CTest::print()
 {
 	std::cout << "hello world!" << std::endl;
 }
+
+void CTest::print2()
+{
+	std::cout << "hello world2!" << std::endl;
+}
--- test.h
+++ test.h
@@ -5,4 +5,6 @@ class CTest
 		~CTest();
 
 		void print();
+
+		void print2();
 };
